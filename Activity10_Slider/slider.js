eb7ecff68f4b49be746c328c9905ea510483f02b
rogrammer <96201583+rogrammer@users.noreply.github.com>
2024-05-10 15:24:13 +0300

    first commit

---
+++ Activity10_Slider/slider.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+		$("#slider").bxSlider({
+			auto: true,
+			slideWidth: 250,
+			slideMargin: 10,
+			randomStart: true,
+			moveSlides: 1,
+			pager: true,
+			pagerType: "short",
+			speed: 300,
+			touchEnabled: false,
+			captions: true
+		});
+    	});
